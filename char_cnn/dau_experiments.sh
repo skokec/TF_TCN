84915a9d797b1e68420dae1c916ede71ab08c568
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2018-11-23 14:51:54 +0000

    Updated dau experiment script

--- char_cnn/dau_experiments.sh
+++ char_cnn/dau_experiments.sh
@@ -2,7 +2,15 @@
 
 # using smaller number of hidden units (256) with additional conv1x1 that combines dau_conv1d output and input
 # this results in similar performance but is faster for development purpuse
-python char_cnn_test.py --gpu=0 --save_path=./output/tDAU/half_gauss_init_std=2_sigma0.5_mulr=100_lr=10 --use_dau --nhid=256 --emsize=128 --epoch=300 --lr=10
+
+# PTB: 1.317 after 300 epoch
+python char_cnn_test.py --gpu=0 --save_path=./output/tDAU/half_gauss_init_std=2_sigma0.5_mulr=100_lr=10 --use_dau --use_conv1x1 --nhid=256 --emsize=128 --epoch=300 --lr=10
+
+# PTB: 1.321 after 300 epoch (using sigma=0.3 and mu_learning_rate_factor=500 that need to be manually changed in the code)
+python char_cnn_test.py --gpu=0 --save_path=./output/tDAU/half_gauss_init_std=2 --use_dau --use_conv1x1 --nhid=256 --emsize=128 --epoch=300 --lr=4
+
 
 # using number of hidden units close to reported paper (416) - no conv1x1
-python char_cnn_test.py --gpu=0 --save_path=./output/tDAU/half_gauss_init_std=2_sigma0.5_mulr=100_lr=10 --use_dau --nhid=416 --emsize=128 --epoch=300 --lr=10
+
+# PTB: 1.322 after 300 epoch (using sigma=0.3 and mu_learning_rate_factor=500 that need to be manually changed in the code)
+python char_cnn_test.py --gpu=0 --save_path=./output/tDAU/half_gauss_feat=416_init_std=2_no_conv1x1 --use_dau --nhid=416 --emsize=128 --epoch=300 --lr=4
